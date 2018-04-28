2620021984120f7d75131bd80fcd56d9f36ca982
Poornima Vasudevan <poorni@Poornimas-MacBook-Air.local>
2018-04-28 18:17:55 -0400

    code for character supply to authentication

--- python-client-master/test/functional/android/outputprogram_amazon.py
+++ python-client-master/test/functional/android/outputprogram_amazon.py
@@ -22,18 +22,64 @@ class MultiActionTests(unittest.TestCase):
         self.dc['reportFormat'] = self.reportFormat
         self.dc['testName'] = self.testName
         self.dc['udid'] = '03ab1c13003c0097'
-        self.dc['appPackage'] = 'com.cnn.mobile.android.phone'
-        self.dc['appActivity'] = '.features.splash.SplashActivity'
+        self.dc['appPackage'] = 'com.amazon.mShop.android.shopping'
+        self.dc['appActivity'] = 'com.amazon.mShop.home.HomeActivity'
         self.dc['platformName'] = 'android'
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub',self.dc)
 
     def testUntitled(self):
-		Launchclick()
-		Authenticate
+		self.driver.find_element_by_xpath("xpath=//*[@id=\'action_bar_burger_icon\']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text=\'Hello. Sign In\']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text=\'Login. Already a customer? \' and @class=\'android.widget.RadioButton\']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='p']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='o']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='o']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='r']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='n']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='i']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='.']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='b']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='a']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='v']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='y']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='a']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='@']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='g']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='m']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='a']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='i']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='l']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='.']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='c']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='o']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='m']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='k']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='a']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='l']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='y']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='a']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='n']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='i']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='p']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='o']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='o']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='r']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='n']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='i']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='m']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text='a']").click()
 		self.driver.find_element_by_xpath("xpath=//*[@id=\'rs_search_src_text\']").click()
-		selecting something
-		Addint to cart
-		Click cart
+		
+		self.driver.find_element_by_xpath("xpath=//*[@text=\'Tap and long press to fling this item to your tray.\' and (./preceding-sibling::* | ./following-sibling::*)[./*[./*[./*[./*[@text=\'Wings of Fire: An Autobiography of APJ Abdul Kalam\']]]]]]").click()
+		
+		self.driver.find_element_by_xpath("xpath=//*[@text=\'Submit\']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text=\'Add to Cart\']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@id=\'action_bar_cart_image\']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text=\'Delete\']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text=\'Settings\']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text=\'Not vasu? Sign out\']").click()
+		self.driver.find_element_by_xpath("xpath=//*[@text=\'Sign Out\']").click()
+		self.driver.press_keycode(3) # home
 
     def tearDown(self):
         self.driver.quit()
--- python-client-master/test/functional/android/programgenerator_amazon.py
+++ python-client-master/test/functional/android/programgenerator_amazon.py
@@ -10,19 +10,38 @@ import subprocess
 fileNameIntent = "intent_amazon.conf"
 fileNameMappings = "mappings_amazon.conf"
 outputFile = "outputprogram_amazon.py"
-templateFile = "automation_template.txt"
+templateFile = "automation_template_amazon.txt"
 automationline = ''
 mappings = collections.defaultdict(list)
 intents = [] 
 
-def addObjectsToProgram (wordIntent_t):
+#
+# Send Key, Value, Parent
+#
+def addObjectsToProgram (wordIntent_t,value_t, parent_t):
     #print "Yes for the intent the key in dict exists\n"
-    automationline = str(mappings[wordIntent_t])
-    automationline = automationline.lstrip('[\' "')
-    automationline = automationline.rstrip('\"\']')
-    fOutput.write("\t\t")
-    fOutput.write(automationline)
-    fOutput.write("\n")
+#    print "...", wordIntent_t, "..."
+    if (parent_t == "credentials"):
+        print parent_t, wordIntent_t, value_t
+        if (wordIntent_t == "username" or  wordIntent_t == "password"):
+            arrayUsername = list(value_t)
+            for character_t in arrayUsername:
+                charFrameLine = "self.driver.find_element_by_xpath(\"xpath=//*[@text='%s']\").click()" %character_t                
+                print charFrameLine
+                fOutput.write("\t\t")
+                fOutput.write(charFrameLine)
+                fOutput.write("\n")
+            print "# [Info] The intent is existinglogin"
+        #else: 
+    elif (wordIntent_t == "authenticate"):
+        print "# [Info] The intent is authenticate"
+    else: 
+        automationline = str(mappings[wordIntent_t])
+        automationline = automationline.lstrip('[\' "')
+        automationline = automationline.rstrip('\"\']')
+        fOutput.write("\t\t")
+        fOutput.write(automationline)
+        fOutput.write("\n")
 
 
 
@@ -80,30 +99,28 @@ if __name__== "__main__":
                         wordIntent = wordIntent.rstrip()
                         wordIntent = wordIntent.lstrip()
                         propertiesArray = []
-                        propertiesArray = wordIntent.split(".")
-                        #print "\n#Properties:"
+                        propertiesArray = wordIntent.split(".",1)
+                        print propertiesArray
                         if (len(propertiesArray) == 1):
-                            print propertiesArray
                             if wordIntent in mappings:
-                                addObjectsToProgram (wordIntent)
-                                continue 
-                        if (len(propertiesArray) >= 2):
+                                #print "Only 1 parameter in the intent %s" %wordIntent
+                                addObjectsToProgram (wordIntent,"none","none")
+                                #continue 
+                        elif (len(propertiesArray) >= 2):
+                            #print "More than 2 parameters in the intent" 
                             for id_t, property_t in enumerate(propertiesArray):
                                 property_t = property_t.rstrip()
                                 property_t = property_t.lstrip()
-                                print id_t, property_t
+                                #print id_t, property_t
                                 if (id_t == 0):
-                                    print "poorni: %s" % property_t
                                     if property_t in mappings:
-                                        print "Eureka found: %s" % property_t
-                                        addObjectsToProgram (property_t)
+                                        addObjectsToProgram (property_t,"none","none")
                                 elif (id_t >= 1):
                                     key_values = []
                                     key_values = property_t.split(",")
                                     for key_val_t in key_values:
                                         key_val = []
                                         key_val = key_val_t.split(":")
-                                        #print key_val
 
                                         key_t = key_val[0]
                                         key = key_t.replace("{", "")
@@ -112,11 +129,11 @@ if __name__== "__main__":
                                             value_t = key_val[1]
                                             value = value_t.replace("}", "")
                                             value = value.replace("{", "")
-                                            print "\t Key: %s, value: %s" %(key, value)
-                                        #print key
+                                            print "\t Key: %s, value: %s Parent: %s" %(key, value, propertiesArray[0])
+                                            addObjectsToProgram (key,value,propertiesArray[0])
                                         else:
                                             print "No key exists for the key: %s" %(key) 
                                             key = 0
 
             
-    #subprocess.Popen(["python", "outputprogram.py"])
+    #subprocess.Popen(["python", "outputprogram_amazon.py"])
